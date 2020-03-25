9423a03a19edfb43f3a90ff1f7b7ff93ebe76427
QuantumInformatik <juanguillermoalarcon@gmail.com>
2020-03-25 07:38:52 -0500

    app propierties se cambia la conexion 3

--- src/main/java/com/inerxiagroup/repository/contacto/ContactoRepository.java
+++ src/main/java/com/inerxiagroup/repository/contacto/ContactoRepository.java
@@ -4,12 +4,15 @@ import com.inerxiagroup.model.contacto.Contacto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
-public interface ContactoRepository extends JpaRepository<Contacto, Integer> {
+@Repository
+public interface ContactoRepository extends CrudRepository<Contacto, Integer> {
     //Optional<Contacto> findByTipoDocumentoAndNumeroDocumento(String tipoDocumento, String numeroDocumento);
 
 
