add31b404c6998258b25ee36200dec8b513a574a
QuantumInformatik <juanguillermoalarcon@gmail.com>
2020-03-24 21:01:44 -0500

    se crea el cross origin

--- src/main/java/com/inerxiagroup/controller/contacto/ContactoController.java
+++ src/main/java/com/inerxiagroup/controller/contacto/ContactoController.java
@@ -8,14 +8,12 @@ import com.inerxiagroup.util.handler.StandardResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.Date;
 
+@CrossOrigin(origins = {"http://localhost:4200"})
 @RestController
 @RequestMapping("/api")
 public class ContactoController {
